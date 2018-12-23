af5f52aa84fdda818ebca4ccfad5686ebb1c8568
JetsenLabs <jj.trujillo@outlook.com>
2018-12-23 03:54:40 -0600

    adding starting project

---
+++ build/precache-manifest.ba8a08b0af766ca45e626728c5cd4905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5726c66b3bde8c420126",
+    "url": "/static/js/main.5726c66b.chunk.js"
+  },
+  {
+    "revision": "04c4add7fafae90f3a89",
+    "url": "/static/js/1.04c4add7.chunk.js"
+  },
+  {
+    "revision": "5726c66b3bde8c420126",
+    "url": "/static/css/main.792cd147.chunk.css"
+  },
+  {
+    "revision": "04c4add7fafae90f3a89",
+    "url": "/static/css/1.7275ca80.chunk.css"
+  },
+  {
+    "revision": "87f07531b1efbd72197290964b546e5e",
+    "url": "/index.html"
+  }
+];
