33773cb7119cb69a143b7d33ffdd2095db757ceb
Trujillo 58 <user@Users-Mac-mini.local>
2018-12-31 12:41:14 -0600

    1st commit from Mac Mini

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import Rank from './components/Rank/Rank.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
-
 import './App.css';
+
+const particlesOptions = {
+            "particles": {
+                "number": {
+                    "value": 160,
+                    "density": {
+                        "enable": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                        "speed": 4,
+                        "size_min": 0.3
+                    }
+                },
+                "line_linked": {
+                    "enable": false
+                },
+                "move": {
+                    "random": true,
+                    "speed": 1,
+                    "direction": "top",
+                    "out_mode": "out"
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "bubble"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "repulse"
+                    }
+                },
+                "modes": {
+                    "bubble": {
+                        "distance": 250,
+                        "duration": 2,
+                        "size": 0,
+                        "opacity": 0
+                    },
+                    "repulse": {
+                        "distance": 400,
+                        "duration": 4
+                    }
+                }
+            }
+        }
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Particles className='particles'
+          params={particlesOptions} />
           <Navigation />
           <Logo />
+          <Rank />
           <ImageLinkForm />
           {/*<FaceRecogition />*/}
       </div>
